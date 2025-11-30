250f227c6bbe7ba755bd2d2f5b3f58329dd0241d
MysticTortoise <shadowknight7788@gmail.com>
2025-11-29 23:07:09 -0700

    Profiles (and assetmixer fix)

--- Subtools/AssetMixer/run.py
+++ Subtools/AssetMixer/run.py
@@ -1,4 +1,4 @@
-import importlib.util, os, sys
+import importlib.util, os, sys, pathlib
 
 def run(asset_source, build_path, asset_platform, parser_dirs : set=set()):
     parser_dirs.add(os.path.dirname(__file__) + "/Parsers/")
@@ -24,10 +24,17 @@ def run(asset_source, build_path, asset_platform, parser_dirs : set=set()):
     default_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(default_module)
 
+
     for root, dirs, files in os.walk(asset_source):
         for filename in files:
             foundSpec = False
             asset_path = os.path.join(root,filename).replace(asset_source, "")[1:]
+
+            src_path = os.path.join(root,filename)
+            dest_path = dest_dir + asset_path
+
+            pathlib.Path(os.path.dirname(dest_path)).mkdir(parents=True, exist_ok=True)
+            
             # SUPER TEMP
             for parser_dir in parser_dirs:
                 script_src = parser_dir + os.path.splitext(asset_path)[1][1:] + ".py"
@@ -36,11 +43,11 @@ def run(asset_source, build_path, asset_platform, parser_dirs : set=set()):
                     spec = importlib.util.spec_from_file_location(parser_path, parser_path)
                     special_module = importlib.util.module_from_spec(spec)
                     spec.loader.exec_module(special_module)
-                    special_module.parse_and_copy(os.path.join(root,filename), dest_dir + asset_path)
+                    special_module.parse_and_copy(src_path, dest_path)
                     foundSpec = True
                     break
             if not foundSpec:
-                default_module.parse_and_copy(os.path.join(root,filename), dest_dir + asset_path)
+                default_module.parse_and_copy(src_path,dest_path)
 
 if __name__ == "__main__":
     run(sys.argv[1], sys.argv[2], sys.argv[3])
